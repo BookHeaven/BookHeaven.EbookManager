40e60a261fe6b19fc03b1e7e8f6f07c6813e902a
heasheartfire <gagi1995@gmail.com>
2026-03-09 02:14:07 +0100

    fix(NAV): enhance toc item text retrieval logic to handle cases where the <a> element doesn't have a direct XmlText node child

--- Formats/Epub/XML/NAV.cs
+++ Formats/Epub/XML/NAV.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
 using System.Xml.Serialization;
 using BookHeaven.EbookManager.Formats.Epub.Constants;
 
@@ -29,6 +30,40 @@ public class NavA
 {
     [XmlAttribute("href")]
     public string Href { get; set; } = null!;
+    
     [XmlText]
-    public string Text { get; set; } = null!;
+    public string SimpleText { get; set; } = string.Empty;
+    
+    [XmlAnyElement]
+    public XmlElement? Child { get; set; }
+
+    [XmlIgnore]
+    public string Text
+    {
+        get
+        {
+            if(!string.IsNullOrEmpty(SimpleText)) return SimpleText;
+            return Child is null ? string.Empty : GetTextFromLink(Child);
+        }
+    }
+    
+    private static string GetTextFromLink(XmlElement element)
+    {
+        var text = string.Empty;
+        if (!element.HasChildNodes) return text;
+        
+        foreach (XmlNode child in element.ChildNodes)
+        {
+            switch (child.NodeType)
+            {
+                case XmlNodeType.Text:
+                    text += child.Value;
+                    break;
+                case XmlNodeType.Element:
+                    text += GetTextFromLink((XmlElement)child);
+                    break;
+            }
+        }
+        return text;
+    }
 }
